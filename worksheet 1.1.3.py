bc79a5f342e0114bd0643a9711c2938716ed54b8
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-02-16 18:00:25 +0530

    Create worksheet 1.1.3.py

---
+++ worksheet 1.1.3.py
@@ -0,0 +1,22 @@
+#3.	Write a  program to enter length in centimeter and convert it into meter and kilometer, and also convert the same into Equivalents 
+
+cm1 = eval(input("Enter length(in cm) : "))
+
+m1 = cm1 / 100
+km1 = m1 / 1000
+
+print("Converted lengths :", m1, "m & ", km1, "km \n")
+
+m2= eval(input("Enter length(in m) : ))
+
+cm2 = m2 * 100
+km2 = m2 / 1000
+
+print("Converted lengths :", cm2, "cm & ", km2, "km \n")
+
+km3 = eval(input("Enter length(in km) : "))
+
+m3 = km3 * 1000
+cm3 = m3 * 100
+
+print("Converted lengths :", m3, "m & ", cm3, "cm")
