92f3f83eba2d96cf2933f4d7b241be0fed1bce51
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-02-17 20:12:40 +0530

    Create worksheet 1.2.2.py

---
+++ worksheet 1.2.2.py
@@ -0,0 +1,15 @@
+#2. 
+
+num = int(input("Enter an integer : "))
+temp = num
+arm = 0
+
+while (num > 0):
+    div = num % 10
+    arm = arm + div ** 3
+    num = n um// 10
+
+if (arm == temp):
+    print(temp, " is an Armstrong")
+else :
+    print(temp, " is not an Armstrong")
