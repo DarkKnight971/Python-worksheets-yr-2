2ee1cb302acf09a5a3327ba4b892a1dfef283403
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-03-07 12:36:30 +0530

    Create worksheet-2.1.2.py

---
+++ worksheet-2.1.2.py
@@ -0,0 +1,17 @@
+#Python program to find uncommon words from two Strings
+
+a = input("Enter first string : ")
+b = input("Enter second string : ")
+
+a = a.split()
+b = b.split()
+c = []
+
+for x in a :
+  if x not in b :
+    c.append(x)
+for x in b :
+  if x not in a :
+    c.append(x)
+    
+print(x)
