aba989d630be27df4f8e9e92a338af627ce155ca
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-10 21:34:48 +0530

    Update worksheet 2.3.1.py

--- worksheet 2.3.1.py
+++ worksheet 2.3.1.py
@@ -1,1 +1,15 @@
+'''Write a Python program to combine two dictionary 
+   adding values for common keys. d1 = {'a': 100, 
+   'b': 200, 'c':300},d2 = {'a': 300, 'b': 200, 
+   'd':400}'''
 
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+
+for key in d2:
+	if key in d1:
+		d2[key] = d2[key] + d1[key]
+	else:
+		pass
+		
+print(d2)
