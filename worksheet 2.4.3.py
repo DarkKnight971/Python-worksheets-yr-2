e873e62c2d1ba3a7dbd6c96cbd51b16c4e60eea5
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-11 21:55:15 +0530

    Update worksheet 2.4.3.py

--- worksheet 2.4.3.py
+++ worksheet 2.4.3.py
@@ -1,2 +1,12 @@
 '''Write a Python program calculate the product,
    multiplying all the numbers of a given tuple.'''
+
+t = 1, 2, 3, 4, 5, 6
+print("Original tuple : ", t)
+
+prod = 1
+t = list(t)
+for i in t :
+   prod *= i
+
+print("Product of elements of tuple : ", prod)
