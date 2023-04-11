ffe6c77b6c9d9ce8197084b614b065113243b0c1
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-11 21:52:08 +0530

    Update worksheet 2.4.2.py

--- worksheet 2.4.2.py
+++ worksheet 2.4.2.py
@@ -1,2 +1,13 @@
 '''Write a Python program to remove an empty
    tuple(s) from a list of tuples'''
+
+t = (), (23, 29), (43, 59, 67)
+print("Original tuple : ", t)
+
+t = list(t)
+for i in t :
+   if not i :
+      t.remove(i)
+t = tuple(t)
+
+print("Updated tuple : ", t)
