1bcc81ba55e91c879192996cf8ab2f96860b045b
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-11 22:16:23 +0530

    Update worksheet 2.4.5.py

--- worksheet 2.4.5.py
+++ worksheet 2.4.5.py
@@ -1,2 +1,14 @@
 '''Write a Python program to check if a
    specified element presents in a tuple of tuples'''
+
+t = (23, 29), (41, 53), (67,79)
+print("Original tuple : ", t)
+
+s = int(input("Enter number to search : "))
+l = []
+
+for i in t :
+   if s in i :
+      l.append(s)
+
+print("Element present" if l else "ELement absent")
