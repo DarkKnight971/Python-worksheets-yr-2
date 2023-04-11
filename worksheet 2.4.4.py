d9af5ba44bd575dc79e6d00acc79850b1aff06b6
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-11 22:03:40 +0530

    Update worksheet 2.4.4.py

--- worksheet 2.4.4.py
+++ worksheet 2.4.4.py
@@ -1,2 +1,10 @@
 '''Write a Python program to convert a tuple of
    string values to a tuple of integer values'''
+
+t = '1', '2', '3', '4', '5'
+print("Original tuple : ", t)
+
+t = [int(i) for i in t]
+t = tuple(t)
+
+print("Updated tuple : ", t)
