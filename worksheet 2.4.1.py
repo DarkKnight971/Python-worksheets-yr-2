47919ffa17b0b6d25f8538fcb83620573c2fc162
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-04-11 21:44:31 +0530

    Update worksheet 2.4.1.py

--- worksheet 2.4.1.py
+++ worksheet 2.4.1.py
@@ -1,1 +1,10 @@
 '''Write a Python program to replace last value of tuples in a list'''
+
+t = 23, 29, 43, 59, 67
+print("Original tuple : ", t)
+t = list(t)
+
+t[-1] = 97
+
+t = tuple(t)
+print("Updated tuple : ", t)
