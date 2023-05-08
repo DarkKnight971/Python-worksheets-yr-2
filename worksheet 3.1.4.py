e8a8fbbd07e864bac6873001aa148589eb969a54
DarkKnight971 <69305872+DarkKnight971@users.noreply.github.com>
2023-05-08 22:33:15 +0530

    Create worksheet 3.1.4.py

---
+++ worksheet 3.1.4.py
@@ -0,0 +1,27 @@
+#4. Python program to implement selection sort.
+
+def main() :
+	data = []
+	n = int(input("Enter size of array : "))
+	
+	for i in range(0, n) :
+		d = int(input("Enter an integer : "))
+		data.append(d)
+	
+	insertionSort(data)
+	print('Sorted Array in Ascending Order:')
+	print(data)
+
+def insertionSort(array, n):
+    for i in range(1, n):
+        key = array[i]
+        j = i - 1
+		
+        while (j >= 0) and (key < array[j]):
+            array[j + 1] = array[j]
+            j = j - 1
+        
+        array[j + 1] = key
+
+if __name__ == '__main__' :
+	main()
